56688868c42407c691e49d504222098a5b4db2dc
xprism1 <50407586+xprism1@users.noreply.github.com>
2020-06-09 12:03:28 +0800

    v1.1

--- files2dat.py
+++ files2dat.py
@@ -35,27 +35,29 @@ def add_rom(datfile, soup, game_name, dict, glo): # adds a new entry under <game
     datfile_added = str(soup)
     with open(datfile, "w+") as f:
         f.write(datfile_added) # writes modifications to datfile
-        
-# Getting user information
-print("files2dat v1.0 by xprism")
-path = str(input("Enter the path to the folder containing files (and files only), e.g. C:\\Users "))
-datfilename = str(input("Enter the path to the datfile, e.g. C:\\datfile.dat "))
 
-global_entries = str(input("Do you want to batch add entries for all the files? E.g. status = 'verified'. [y/n] "))
+# Getting user information
+print("files2dat v1.1 by xprism")
+print("WARNING: Do not put the python scripts or the datfile inside the folder with files you want to add. Quit the program now and move them out if so.")
+path = str(input("Enter the path to the folder containing files (and files only), e.g. C:\\Users: "))
+datfilename = str(input("Enter the path to the datfile, e.g. C:\\datfile.dat: "))
+datfileoutput = str(input("Enter the path to the output file, e.g. C:\\datfile_modified.dat: "))
+
+global_entries = str(input("Do you want to batch add entries for all the files? E.g. status = 'verified'. [y/n]: "))
 add_more = True
 glo = {}
 if global_entries == "y":
     while(add_more == True):
-        key = str(input("Enter the entry key, e.g. in status = 'verified', status is the key and 'verified' is the value. "))
-        value = str(input("Enter the entry value, e.g. in status = 'verified', status is the key and 'verified' is the value. "))
-        cont = str(input("Continue adding entries? [y/n] "))
+        key = str(input("Enter the entry key, e.g. if you want to have status = 'verified', enter status: "))
+        value = str(input("Enter the entry value, e.g. if you want to have status = 'verified', enter verified (without the quotes): "))
+        cont = str(input("Continue adding entries? [y/n]: "))
         glo[key] = value
         if cont == 'n':
             add_more = False
-            
+
 datfile = open(datfilename, "r")
 xml_data = datfile.read()
-soup = BeautifulSoup(xml_data, 'lxml')     
+soup = BeautifulSoup(xml_data, 'lxml')
         
 from getfileinfo import fileinfo # import fileinfo function from getfileinfo.py
 print("Getting file info...")
@@ -74,4 +76,28 @@ for i in range(len(info)):
     add_rom(datfilename, soup, filename_wo_ext, info[i], glo)
 
 print("Datfile modified successfully.")
-print("Please open the datfile in a text editor manually, and remove <html> (at the start of the file) as well as </html> (at the end of the file).")
+
+from lxml import etree
+
+# Resetting the already existing indentation, allowing the output to generate it's own indentation correctly
+parser = etree.XMLParser(remove_blank_text=True)
+tree = etree.parse(datfilename, parser)
+
+# Reformat datfile
+with open(datfileoutput, "wb") as f:
+    tree.write(f, pretty_print=True)
+    
+print("Datfile prettified successfully.")
+
+# Adds <?xml version> and <!DOCTYPE> to start of output file
+def line_prepender(filename, line):
+    with open(filename, 'r+') as f:
+        content = f.read()
+        f.seek(0, 0)
+        f.write(line.rstrip('\r\n') + '\n' + content)
+        
+line_prepender(datfileoutput, '<!DOCTYPE datafile PUBLIC "-//Logiqx//DTD ROM Management Datafile//EN" "http://www.logiqx.com/Dats/datafile.dtd">')
+line_prepender(datfileoutput, '<?xml version="1.0"?>')
+
+print("Output file: " + datfileoutput)
+print("Please open the datfile in a text editor manually, and remove <html>, <body> (at the start of the file) as well as </body>, </html> (at the end of the file).")
